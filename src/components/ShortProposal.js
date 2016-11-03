e3a18c685921b42aa660c4cf44f42b08d482e994
Brett Hevia <brett.hevia@gmail.com>
2016-11-03 17:33:44 -0600

    adds some more styles to home page

--- src/components/ShortProposal.js
+++ src/components/ShortProposal.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 const ShortProposal = (proposal) => {
   return (
     <li
-    className="shortened-proposal"
+    className="short-proposal"
     onClick={e => proposal.grabProposal(proposal)}>
       <h2 className="short-title">{proposal.title}</h2>
       <h3 className="short-author">{proposal.author}</h3>
