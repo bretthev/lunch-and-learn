156b060eb2b0a848cb7c706f7045069f03309520
Brett Hevia <brett.hevia@gmail.com>
2016-11-14 16:46:11 -0500

    disabled buttons on finalize presentation form

--- src/components/ProposalForm.js
+++ src/components/ProposalForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ProposalList from '../containers/ProposalList'
 import Navigation from '../containers/Navigation';
-// import { Link } from 'react-router';
+import { Link } from 'react-router';
 
 
 export default class ProposalForm extends React.Component {
@@ -46,35 +46,3 @@ export default class ProposalForm extends React.Component {
     )
   }
 }
-// const ProposalForm = ({currentUser, submitProposal, targetProposal, buttonText}) => {
-//   let proposalTitle;
-//   let proposalBody;
-//   let isDisabled = true;
-//
-//   const updateIsDisabled = () => {
-//     if (proposalTitle.value !== '' && proposalBody.value !== '') { isDisabled = false; }
-//     else { isDisabled = true }
-//   }
-//
-  // return (
-  //   <section className="add-proposal">
-  //     <h1 className="form-title">Add your proposal</h1>
-  //     <form className="add-proposal-form"
-  //       onSubmit={ (e) => {
-  //         e.preventDefault()
-  //         submitProposal({author: currentUser.username, title: proposalTitle.value, body: proposalBody.value, timestamp: Date.now(), id: targetProposal.id,  })
-  //         proposalTitle.value='';
-  //         proposalBody.value='';
-  //       }}>
-  //       <input placeholder="Proposal Title" className="title-input" defaultValue={targetProposal.title} ref={ node => { proposalTitle = node}} onChange={e => updateIsDisabled()}/>
-  //       <textarea placeholder="Proposal Content" className="body-input" defaultValue={targetProposal.body} ref={ node => { proposalBody = node}} onChange={e => updateIsDisabled()}/>
-  //       <button className="submit-proposal-button" disabled={isDisabled}>
-  //         {buttonText}
-  //       </button>
-  //     </form>
-  //     <Navigation/>
-  //   </section>
-//   )
-// }
-//
-// export default ProposalForm;
